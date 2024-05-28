b1df70e267411e2abb7c9ff787f3559e52b89670
evren.ispiroglu <evren.ispiroglu@trendyol.com>
2024-05-28 22:19:05 +0300

    Initial commit

---
+++ src/main/java/org/secapcompass/goalsplansapi/GoalsPlansApiApplication.java
@@ -0,0 +1,13 @@
+package org.secapcompass.goalsplansapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalsPlansApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoalsPlansApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/secapcompass/goalsplansapi/GoalsPlansApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.secapcompass.goalsplansapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalsPlansApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
